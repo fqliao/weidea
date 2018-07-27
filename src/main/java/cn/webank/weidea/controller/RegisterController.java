7648ac87da049b92f95638f58bcc2bcebe7dc363
lukelu(卢璐) <lukelu@webank.com>
2018-07-27 09:27:31 +0800

    add controller

---
+++ src/main/java/cn/webank/weidea/controller/RegisterController.java
@@ -0,0 +1,44 @@
+package cn.webank.weidea.controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.webank.weidea.entity.User;
+
+@Controller
+public class RegisterController {
+	
+	@ResponseBody	
+	@RequestMapping(value="api/register")
+	public boolean register(HttpServletRequest req,HttpServletResponse res){
+		
+		User user=new User();
+		user.setIdCard(req.getParameter("idCard"));
+		user.setUsername(req.getParameter("userName"));
+		user.setPassword(req.getParameter("password"));
+		user.setPhone(req.getParameter("phone"));
+		if(req.getParameter("sex")!=null) {
+			user.setSex(Integer.parseInt(req.getParameter("sex")));
+		}
+			
+		//ceshi
+		System.out.println(user.toString());
+		
+		//加密
+		
+		//调sdk上链	
+	
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+}
