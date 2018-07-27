a93959f079e7bf660000b92eeb31a01b1adc5961
grayguo <grayguo@grayguo-nb0.webank.com>
2018-07-27 22:42:56 +0800

    fininsh SecretKey

--- src/main/java/cn/webank/weidea/controller/Test.java
+++ src/main/java/cn/webank/weidea/controller/Test.java
@@ -1,16 +1,24 @@
 package cn.webank.weidea.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.webank.weidea.dao.UserRepository;
+
 @Controller
 public class Test {
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@RequestMapping(value = "aaa")
 	@ResponseBody
 	public String test(@RequestParam(name = "ddd", required = false) String ddd) {
 		System.out.println(ddd);
+		System.out.println("count=:" + userRepository.count());
 		return "zzzz";
 	}
 }
