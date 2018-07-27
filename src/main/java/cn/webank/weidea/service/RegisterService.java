157d29bbafa8aabcf4cd8ad730752b78c91a3c32
lukelu(卢璐) <lukelu@webank.com>
2018-07-27 17:36:57 +0800

    modify

--- src/main/java/cn/webank/weidea/service/RegisterService.java
+++ src/main/java/cn/webank/weidea/service/RegisterService.java
@@ -1,13 +1,18 @@
 package cn.webank.weidea.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cn.webank.weidea.controller.RegisterController;
 import cn.webank.weidea.dao.UserRepository;
 import cn.webank.weidea.entity.User;
 
 @Service
 public class RegisterService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterService.class);
 	
 	@Autowired
 	UserRepository userRepository;	
@@ -19,11 +24,14 @@ public class RegisterService {
 		varifyUserInfo(user);
 		if(userRepository.findPublishKey(user.getIdCard())==null) {
 			String pkey=getPulibcKeyFromPKI(user.getIdCard());
+			LOGGER.info("从第三方机构获取公钥"+pkey);
 			user.setPublishKey(pkey);
 			//加密
 			
 			//上链
-			//userRepository.save(user);
+			userRepository.save(user);
+			LOGGER.info("用户信息密文上链成功");
+			System.out.println("用户信息密文上链成功");
 			
 			//传输口令给第三方
 			return true;
