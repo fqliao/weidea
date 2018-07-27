cae82d2a9f183b0af4193977ebce4eb671a96ed0
grayguo <grayguo@grayguo-nb0.webank.com>
2018-07-27 17:35:04 +0800

    add sl4j

--- src/main/java/cn/webank/weidea/controller/Test.java
+++ src/main/java/cn/webank/weidea/controller/Test.java
@@ -2,11 +2,15 @@ package cn.webank.weidea.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Test {
 	@RequestMapping(value = "aaa")
-	public String test() {
+	@ResponseBody
+	public String test(@RequestParam(name = "ddd", required = false) String ddd) {
+		System.out.println(ddd);
 		return "zzzz";
 	}
 }
